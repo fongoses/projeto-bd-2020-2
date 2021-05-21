d781ace1a44bc370ffcc60042404a00acb3a7863
Luiz Souza <fongoses@gmail.com>
2021-05-21 04:47:26 -0300

    Conexão OK

--- consulta1.php
+++ consulta1.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+error_reporting(E_ALL);
+?>
 <html lang="pt-br">
 <head>
 <meta charset="utf-8">
@@ -21,13 +24,44 @@
         <?php
         require_once __DIR__ . "/vendor/autoload.php";
 
-        $client = new MongoDB\Client("mongodb+srv://root:@localhost/?serverSelectionTryOnce=false&serverSelectionTimeoutMS=15000&w=majority");
+        $client = new MongoDB\Client("mongodb://root:@localhost/");
 
-        $db = $client->acerpi;
+        $db = $client->selectDatabase("acerpi");
 
-        var_dump($db);
+        //var_dump($db);
 
-        ?>
+        $collection = $db->selectCollection("ufrgs-records");
+
+        //var_dump($collection);
+
+       ?>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Nome Assinatura</th>
+                <th>Matrículas SIAPE do Documento</th>
+                <th>Nome do Arquivo</th>
+            </tr>
+            <?php
+            echo "teste 1";
+            $portarias = $collection->find();
+            echo "teste 2";
+
+            var_dump($portarias);
+
+            foreach($portarias as $p) {
+                echo "<tr>";
+                echo "<td>" . $p->id . "</td>";
+                //echo "<td>" . $p->name . "</td>";
+                //echo "<td>" . implode($p->siape) . "</td>";
+                //echo "<td>" . $p->document->name . "</td>";
+                echo "</tr>";
+            };
+
+
+            ?>
+        </table>
+
     </div>
     <br>
 
