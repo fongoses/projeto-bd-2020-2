b23407303f48b2e5027f0f77074323ab49cfabba
Luiz Souza <fongoses@gmail.com>
2021-05-25 05:15:17 -0300

    Versão uncional em PHP simples

--- consulta1.php
+++ consulta1.php
@@ -24,44 +24,54 @@ error_reporting(E_ALL);
         <?php
         require_once __DIR__ . "/vendor/autoload.php";
 
-        $client = new MongoDB\Client("mongodb://root:@localhost/");
+        if(!isset($_GET["txtId"]) || ($_GET["txtId"]=="")) exit;
+        $id = intval($_GET["txtId"]);
+
+        $client = new MongoDB\Client("mongodb://localhost:27017");
 
         $db = $client->selectDatabase("acerpi");
 
-        //var_dump($db);
-
         $collection = $db->selectCollection("ufrgs-records");
 
-        //var_dump($collection);
+        $query = [
+            'id' => $id
+        ];
+
+        $options = [];
 
        ?>
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>Nome Assinatura</th>
-                <th>Matrículas SIAPE do Documento</th>
-                <th>Nome do Arquivo</th>
-            </tr>
-            <?php
-            echo "teste 1";
-            $portarias = $collection->find();
-            echo "teste 2";
+        <div><h1>Parâmetros da Consulta</h1></div>
+        <div>
+            <div><span>ID do Registro:</span> <?=$id ?></div>
+        </div>
+        <br>
 
-            var_dump($portarias);
+        <div>
+            <table>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome Assinatura</th>
+                    <th>Matrículas SIAPE do Documento</th>
+                    <th>Nome do Arquivo</th>
+                </tr>
+                <?php
+                $portarias = $collection->find($query, $options);
 
-            foreach($portarias as $p) {
-                echo "<tr>";
-                echo "<td>" . $p->id . "</td>";
-                //echo "<td>" . $p->name . "</td>";
-                //echo "<td>" . implode($p->siape) . "</td>";
-                //echo "<td>" . $p->document->name . "</td>";
-                echo "</tr>";
-            };
+                foreach($portarias as $p) {
+                    foreach($p["siape"] as $s) {
+                        $siape[] = $s;
+                    }
 
-
-            ?>
-        </table>
-
+                    echo "<tr>";
+                    echo "<td>" . $p->id . "</td>";
+                    echo "<td>" . $p->name . "</td>";
+                    echo "<td>" . implode(", ", $siape) . "</td>";
+                    echo "<td>" . $p->document->name . "</td>";
+                    echo "</tr>";
+                };
+                ?>
+            </table>
+        </div>
     </div>
     <br>
 
